b96a64e5b5aa9e6b7a7e823a652337a0685f6b32
astian4 <sebastianmuchui79@gmail.com>
2022-05-20 11:08:23 +0300

    Better Arrangement

---
+++ Desktop/inspire-in-stem/stem-training/01_intro/main.py
@@ -0,0 +1,46 @@
+my_age = 19
+my_name = "Sebastian"
+# print("My age is" ,my_age)
+strin1 = "Sebastian "
+strin2 = "muchui"
+people = 40
+mult = people*2
+# print(mult)
+# a=4
+# b=10
+
+# mod = a%b
+# add = a+b
+# sub = b-a
+# div = b/a
+
+# print(mod)
+# print(add)
+# print(sub)
+# print(div)
+
+b = "2"
+a = 5
+a = b*a
+# print(a)
+
+"""
+ASSIGNMENT
+get input from user
+
+Build a calculator
+"""
+
+# print("What is your name")
+# name = input()
+# print("your name is  :",name)
+
+# Build a calculator 
+print("Enter the first number")
+num1 = input("first number:")
+
+print("Enter the second number")
+num2 = input("Second number :")
+
+sum = int(num1)+int(num2)
+print(sum)
---
+++ Desktop/inspire-in-stem/stem-training/control-flow-2/while_loops.py
@@ -0,0 +1,8 @@
+i = 0
+while i<=15:
+    
+    i+=1
+    if i>5 and i<10:
+        break
+    print(i)
+print("End of loop")    
