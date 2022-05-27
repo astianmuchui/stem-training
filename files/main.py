de71585eec275a932396b5c9c1e8c377dc9122e8
astian4 <sebastianmuchui79@gmail.com>
2022-05-27 08:39:42 +0300

    File handling lesson

--- files/main.py
+++ files/main.py
@@ -1,8 +1,23 @@
-file = open('./roles.txt','r')
-words = file.read()
-print(words)
 
+# Open a file
+myFile = open('roles.txt', 'w')
 
-# ASST
-#  r , w, r+ a
-# try exceptt
+# Get some info
+print('Name: ', myFile.name)
+print('Is Closed : ', myFile.closed)
+print('Opening Mode: ', myFile.mode)
+
+# Write to file
+myFile.write('I love Python')
+myFile.write(' and JavaScript')
+myFile.close()
+
+# Append to file
+myFile = open('myfile.txt', 'a')
+myFile.write(' I also like PHP')
+myFile.close()
+
+# Read from file
+myFile = open('myfile.txt', 'r+')
+text = myFile.read(100)
+print(text)
